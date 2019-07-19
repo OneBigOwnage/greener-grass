1725154ea30c72dfadd872929aefd8346d36b16d
OneBigOwnage <niekvandenbos@hotmail.com>
2019-07-19 18:26:17 +0200

    Add some tests, which were created in the process of messing around with the packages and the newly created service.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,100 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\GitHubContributionsService;
+
+use Weidner\Goutte\GoutteFacade;
+use GrahamCampbell\GitHub\GitHubManager;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function it_can_authenticate_with_github()
     {
-        $response = $this->get('/');
+        /** @var GitHubManager $github */
+        $github = app()->make(GitHubManager::class);
 
-        $response->assertStatus(200);
+        $issueInformation = $github->issues()->show('SunetiOnlineSolutions', 'bulk-cargo', 12);
+
+        $this->assertEquals('patrickvanwijk', $issueInformation['user']['login']);
+    }
+
+    /** @test */
+    public function it_can_create_an_issue_on_github()
+    {
+        /** @var GitHubManager $github */
+        $github = app()->make(GitHubManager::class);
+
+        $github->issues()->create('OneBigOwnage', 'greener-grass', [
+            'title' => 'Second issue created using code!'     ,
+            'body'  => 'This is the body of the second issue.',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_retrieve_the_contributions_of_a_user()
+    {
+        /** @var \Symfony\Component\DomCrawler\Crawler $crawler */
+        $crawler = GoutteFacade::request('GET', 'https://github.com/OneBigOwnage');
+
+        $date = now()->format('Y-m-d');
+
+        $contributions = $crawler->filter("rect[data-date=\"{$date}\"]")->first()->attr('data-count');
+
+        $this->assertEquals(7, $contributions);
+    }
+
+    /** @test */
+    public function the_service_can_retrieve_contributions_count()
+    {
+        // Given
+        // - We have an instance of the service;
+        /** @var GitHubContributionsService $service */
+        $service = app()->make(GitHubContributionsService::class);
+
+        // When
+        // - We ask the number of contributions for a user;
+        $contributions = $service->getContributions('OneBigOwnage');
+
+        // Then
+        // - It should return a number;
+        $this->assertIsNumeric($contributions);
+    }
+
+    /** @test */
+    public function it_counts_existing_issues()
+    {
+        // Given
+        // - We have an instance of the service;
+        /** @var GitHubContributionsService $service */
+        $service = app()->make(GitHubContributionsService::class);
+
+        // When
+        // - We ask the number of issues that have been created in a given repository;
+        $contributions = $service->getIssueCount('OneBigOwnage', 'greener-grass');
+
+        // Then
+        // - It should return a number;
+        $this->assertIsNumeric($contributions);
+    }
+
+    /** @test */
+    public function it_adds_an_automatic_contribution()
+    {
+        // Given
+        // - We have an instance of the service;
+        // - We know the initial issue count;
+        /** @var GitHubContributionsService $service */
+        $service = app()->make(GitHubContributionsService::class);
+        $issueCount = $service->getIssueCount('OneBigOwnage', 'greener-grass');
+
+        // When
+        // - We ask the service to create an issue;
+        $service->addContribution('OneBigOwnage', 'greener-grass');
+
+        // Then
+        // - The issue count should be upped by one;
+        $this->assertSame($issueCount + 1, $service->getIssueCount('OneBigOwnage', 'greener-grass'));
     }
 }
